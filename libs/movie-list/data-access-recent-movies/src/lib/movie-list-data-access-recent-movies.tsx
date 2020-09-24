1dece29be634a79163d444787e9ada87587cb705
rishabhvallecha <rishabhvallecha@gmail.com>
2020-09-24 22:03:23 +0530

    Added Latest Movie Component and required CSS

--- libs/movie-list/data-access-recent-movies/src/lib/movie-list-data-access-recent-movies.tsx
+++ libs/movie-list/data-access-recent-movies/src/lib/movie-list-data-access-recent-movies.tsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { MovieListUiMovieTile } from '@myorg/movie-list/ui-movie-tile';
 
 import './movie-list-data-access-recent-movies.css';
+
+
+
 import axios from 'axios'
 
 /* eslint-disable-next-line */
 export interface MovieListDataAccessRecentMoviesProps {
-  id: number,
-  title: string,
-  posterPath?: string,
+
 }
 
-export const MovieListDataAccessRecentMovies = () => {
+export interface MovieListDataAccessRecentMovies {
+  id: number,
+  title: string,
+  poster_path?: string,
+  vote_average?: number
+}
 
-  const [latestMovies, setLatestMovies] = useState<MovieListDataAccessRecentMoviesProps[]>([]);
+export const MovieListDataAccessRecentMovies = (props: MovieListDataAccessRecentMoviesProps) => {
+
+  const [latestMovies, setLatestMovies] = useState<MovieListDataAccessRecentMovies[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>('');
 
@@ -41,7 +50,7 @@ export const MovieListDataAccessRecentMovies = () => {
       setLoading(true);
       try {
         const response = await axios(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`)
-        setLatestMovies([...response.data.results])
+        setLatestMovies([...response.data.results.slice(0, 6)])
       }
       catch (error) {
         setErrorMessage(error)
@@ -58,17 +67,9 @@ export const MovieListDataAccessRecentMovies = () => {
 
       {errorMessage && <div>Something went wrong</div>}
       {loading ? <div>Loading. Please Wait.</div> : 
-        <ul>
-          {latestMovies.map(movie => {
-            const { id: movieID, title: movieTitle } = movie
-            return (
-              <li key={movieID}>
-                {movieTitle}
-              </li>
-            )
-          })}
-        </ul>
+        <MovieListUiMovieTile latestMoviesList = {latestMovies} />
       }   
+      
     </div>
   );
 };
--- libs/movie-list/ui-movie-tile/src/lib/movie-list-ui-movie-tile.tsx
+++ libs/movie-list/ui-movie-tile/src/lib/movie-list-ui-movie-tile.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
 import './movie-list-ui-movie-tile.css';
+import { MovieListDataAccessRecentMovies } from '@myorg/movie-list/data-access-recent-movies'
 
 /* eslint-disable-next-line */
-export interface MovieListUiMovieTileProps {}
+export interface MovieListUiMovieTileProps {
+  latestMoviesList: MovieListDataAccessRecentMovies[]
+}
 
 export const MovieListUiMovieTile = (props: MovieListUiMovieTileProps) => {
   return (
-    <div>
-      <h1>Welcome to movie-list-ui-movie-tile!</h1>
+    <div className="movie-list-container">
+        {props.latestMoviesList.map(movie => (
+          <div className="movie-list-item" key={movie.id}>         
+              <img src={`http://image.tmdb.org/t/p/w185${movie.poster_path}`} alt="Poster"/>
+              <div className="movie-title">{movie.title}</div>
+              <div className="movie-title">{movie.vote_average}</div>
+          </div>
+    ))}
     </div>
   );
 };
